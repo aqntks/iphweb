0cf56d01b01db751605810438cb253df64a09253
aqntks <42826598+aqntks@users.noreply.github.com>
2020-06-18 17:43:22 +0900

    등록, 수정, 조회 jpa 코드 작성 등록, 수정, 조회 test코드 작성 h2 웹 콘솔 사용 코드 작성

---
+++ src/main/java/com/iph/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.iph.web.dto;
+
+import com.iph.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
